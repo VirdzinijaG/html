1f8aa0bc094ceef2c741fc014ec675845d375684
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-08-11 10:42:37 +0300

    readFile template handlebars

--- index.js
+++ index.js
@@ -1,33 +1,69 @@
+import * as fs from "fs/promises"; // failu skaitumui
 import handlebars from "handlebars"; // handlebars importavimas
 
-const html = "<p>{{vardas}} {{pavarde}}</p>"; // html turinys
+async function main() {
+    const html = await fs.readFile("testas.handlebars", { // is failo skaitomas tekstas
+        encoding: "utf-8"
+    });
 
+    const template = handlebars.compile(html); // kompiliavimas // sudaro sablona, kuri butu galima nedelsiant vykdyti
 
-const template = handlebars.compile(html); // kompiliavimas // sudaro sablona, kuri butu galima nedelsiant vykdyti
+    const zm = [
+        {
+            vardas: "Jonas",
+            pavarde: "Jonaitis"
+        },
+        {
+            vardas: "Petras",
+            pavarde: "Petraitis"
+        },
+        {
+            vardas: "Antanas",
+            pavarde: "Antanaitis"
+        },
+        {
+            vardas: "Ona",
+            pavarde: "Onaitiene"
+        }
+    ]
 
-const zm = [
-    {
-        vardas: "Jonas",
-        pavarde: "Jonaitis"
-    },
-    {
-        vardas: "Petras",
-        pavarde: "Petraitis"
-    },
-    {
-        vardas: "Antanas",
-        pavarde: "Antanaitis"
-    },
-    {
-        vardas: "Ona",
-        pavarde: "Onaitiene"
+    for (const zmogus of zm) { // for of pereina peer masyva
+        let rez = template(zmogus);
+        console.log(rez);
     }
-]
-
-for (const zmogus of zm) { // for of pereina peer masyva
-    let rez = template(zmogus);
-    console.log(rez);
 }
+
+main();
+
+
+// const html = "<p>Vardas: {{vardas}} Pavarde: {{pavarde}}</p>"; // html turinys
+
+
+// const template = handlebars.compile(html); // kompiliavimas // sudaro sablona, kuri butu galima nedelsiant vykdyti
+
+// const zm = [
+//     {
+//         vardas: "Jonas",
+//         pavarde: "Jonaitis"
+//     },
+//     {
+//         vardas: "Petras",
+//         pavarde: "Petraitis"
+//     },
+//     {
+//         vardas: "Antanas",
+//         pavarde: "Antanaitis"
+//     },
+//     {
+//         vardas: "Ona",
+//         pavarde: "Onaitiene"
+//     }
+// ]
+
+// for (const zmogus of zm) { // for of pereina peer masyva
+//     let rez = template(zmogus);
+//     console.log(rez);
+// }
 // perejus per zm masyva, pasinaudojus sukompiliuotu sablonu, atvaizduojamas html turinys su vardais ir pavardemis, kurie aprasyti zm masyve
 
 // let rez = template({
