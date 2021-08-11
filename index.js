ae1b46716aea4fa91f3c40bbd8592904cbedaf25
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-08-11 10:16:05 +0300

    compile for of

--- index.js
+++ index.js
@@ -5,9 +5,34 @@ const html = "<p>{{vardas}} {{pavarde}}</p>"; // html turinys
 
 const template = handlebars.compile(html); // kompiliavimas // sudaro sablona, kuri butu galima nedelsiant vykdyti
 
-let rez = template({
-    vardas: "Jonas",
-    pavarde: "Jonaitis"
-});
+const zm = [
+    {
+        vardas: "Jonas",
+        pavarde: "Jonaitis"
+    },
+    {
+        vardas: "Petras",
+        pavarde: "Petraitis"
+    },
+    {
+        vardas: "Antanas",
+        pavarde: "Antanaitis"
+    },
+    {
+        vardas: "Ona",
+        pavarde: "Onaitiene"
+    }
+]
 
-console.log(rez);
+for (const zmogus of zm) { // for of pereina peer masyva
+    let rez = template(zmogus);
+    console.log(rez);
+}
+// perejus per zm masyva, pasinaudojus sukompiliuotu sablonu, atvaizduojamas html turinys su vardais ir pavardemis, kurie aprasyti zm masyve
+
+// let rez = template({
+//     vardas: "Jonas",
+//     pavarde: "Jonaitis"
+// });
+
+// console.log(rez);
